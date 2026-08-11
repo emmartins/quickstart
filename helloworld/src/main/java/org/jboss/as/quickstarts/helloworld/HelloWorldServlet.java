238de11ba97531be972ee9a38a5f24a95162e6fe
Eduardo Martins <emartins@redhat.com>
2026-08-11 14:08:10 +0100

    ...

--- helloworld/src/main/java/org/jboss/as/quickstarts/helloworld/HelloWorldServlet.java
+++ helloworld/src/main/java/org/jboss/as/quickstarts/helloworld/HelloWorldServlet.java
@@ -46,7 +46,7 @@ public class HelloWorldServlet extends HttpServlet {
         resp.setContentType("text/html");
         PrintWriter writer = resp.getWriter();
         writer.println(PAGE_HEADER);
-        writer.println("<h1>Hello World!</h1>");
+        writer.println("<h1>Hello World "+System.getProperty("test")+"!</h1>");
         writer.println(PAGE_FOOTER);
         writer.close();
     }
